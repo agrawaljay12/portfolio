bf1ea84248ba199f1b7b5e9a0014b0e1a7596d66
agrawaljay12 <agrawaljay247@gmail.com>
2026-05-17 18:45:50 +0530

    refactor the download functionality

--- frontend/src/pages/About.tsx
+++ frontend/src/pages/About.tsx
@@ -28,6 +28,7 @@ const About = () => {
           <a
             href={cv_path}
             download
+            target="_blank"
             className="inline-block bg-blue-600 text-white 
                        px-6 py-3 rounded-lg font-medium 
                        hover:bg-blue-700 transition shadow-sm hover:shadow-md"
